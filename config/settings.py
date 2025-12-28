9d76462beb967e3bbcda20ee8b281ba566332073
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-12-28 15:37:30 -0500

    feat: add User models and roles

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "users.User"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
