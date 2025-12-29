95c96ad821bd80896c593f08dbbfdf7317adc9c9
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-12-29 11:35:50 -0500

    feat: add register customer user

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views.jwt import EmailTokenObtainPairView
+from .views.register import RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path("login/", EmailTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", RegisterView.as_view(), name="register_user"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
